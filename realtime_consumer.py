f202223d204f487c229cfb499628974932de4150
Sree Sanjeev <32449066+sreesanjeevkg@users.noreply.github.com>
2023-09-29 22:37:18 -0500

    Update realtime_consumer.py

--- realtime_consumer.py
+++ realtime_consumer.py
@@ -9,9 +9,7 @@ kafka_helper = KafkaHelper()
 realtime_logger = logging.getLogger('realtime-system')
 kafka_helper.setup_logger(logger_level=logging.INFO, logger_filename='realtime.log', logger_instance=realtime_logger)
 
-BOOTSTRAP_SERVERS = 'localhost:29092'
-
-consumer_real_time = Consumer({'bootstrap.servers': [BOOTSTRAP_SERVERS]
+consumer_real_time = Consumer({'bootstrap.servers': 'localhost:9092'
                                   , 'group.id': 'consumer-realtime'
                                   , 'auto.offset.reset': 'earliest'
                                   , 'auto.commit.interval.ms': 1000
@@ -22,20 +20,24 @@ realtime_logger.info('Available topics to consume: %s', consumer_real_time.list_
 consumer_real_time.subscribe(['user-login'])
 realtime_logger.info("Polling user-login topic")
 
-while True:
-    msg = consumer_real_time.poll()
-    if msg is None:
-        realtime_logger.info('No message received')
-        continue
-    if msg.error():
-        realtime_logger.info('Error: {}'.format(msg.error(), msg.topic(), msg.partition(), msg.offset()))
-        continue
-    data = msg.value().decode('utf-8')
-    data = json.loads(data)
-    ## TODO: Send it for realtime processing - like frontend or oltp
-    realtime_logger.info(data)
+def main():
+    while True:
+        msg = consumer_real_time.poll()
+        if msg is None:
+            realtime_logger.info('No message received')
+            continue
+        if msg.error():
+            realtime_logger.info('Error: {}'.format(msg.error(), msg.topic(), msg.partition(), msg.offset()))
+            continue
+        data = msg.value().decode('utf-8')
+        data = json.loads(data)
+        ## TODO: Send it for realtime processing - like frontend or S3
+        realtime_logger.info(data)
 
-consumer_real_time.close()
+    consumer_real_time.close()
 
-
-
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        realtime_logger.info('Consumer interrupted by user')
